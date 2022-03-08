c989e54f0a403866305794ee31555b6c54a57966
Nurul Ismat Tanni <57992438+ismat-tanni@users.noreply.github.com>
2022-03-08 23:50:31 +0800

    Add files via upload

---
+++ Basic practice of java/jspiterator/bean/MobilePhone.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ *
+ * @author GATES
+ */
+public class MobilePhone {
+    private String name;
+    private String brand;
+    private double price;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}

4dcaada388ec0a48bd25702ee70d188ad10224ed
Nurul Ismat Tanni <57992438+ismat-tanni@users.noreply.github.com>
2022-03-08 23:03:59 +0800

    Update ImageServlet.java

--- Basic practice of java/ImageServlet.java
+++ Basic practice of java/ImageServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author GATES
+ * @author ISMAT TANNI
  */
 @WebServlet(urlPatterns = {"/ImageServlet"})
 public class ImageServlet extends HttpServlet {

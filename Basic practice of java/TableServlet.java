13835c48b9a26bd19728e2972f5a54dd9fa2111a
Nurul Ismat Tanni <57992438+ismat-tanni@users.noreply.github.com>
2022-03-08 23:04:53 +0800

    Update TableServlet.java

--- Basic practice of java/TableServlet.java
+++ Basic practice of java/TableServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author GATES
+ * @author ISMAT TANNI
  */
 @WebServlet(urlPatterns = {"/TableServlet"})
 public class TableServlet extends HttpServlet {
